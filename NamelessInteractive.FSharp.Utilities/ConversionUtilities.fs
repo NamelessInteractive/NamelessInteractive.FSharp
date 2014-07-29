b03199e6d6b425fe47e72c966e7dac9a0b09e273
Kevin Ashton <zaowuya@gmail.com>
2014-07-29 10:39:19 +0200

    Added some conversion utilties that I frequently use.

---
+++ NamelessInteractive.FSharp.Utilities/ConversionUtilities.fs
@@ -0,0 +1,23 @@
+﻿module NamelessInteractive.FSharp.Utilities.ConversionUtilities
+
+open NamelessInteractive.FSharp.Utilities.Maybe
+
+let InvariantCulture = System.Globalization.CultureInfo.InvariantCulture
+
+let As<'T> (parseFunc : string -> 'T) (value: string option)  =
+    maybe {
+        let! x = value
+        return parseFunc(x)
+    }
+
+let AShort (value: string) =
+    int16 value
+
+let ALong (value: string) =
+    int64 value
+
+let ADecimal (value: string) =
+    System.Convert.ToDecimal(value, InvariantCulture)
+
+let ADate (value: string) =
+    System.DateTime.Parse(value, InvariantCulture)
